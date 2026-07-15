4411040525973ac22edff2d8e1f4ce2f3f6eec30
Suwijak123 <1suwijakt@gmail.com>
2026-07-15 10:53:45 +0700

    Quiz1

--- Output1.dart
+++ Output1.dart
@@ -1,3 +1,21 @@
 void main(){
-  print("Hello");
+
+  String course = 'Flutter Fundamentals';
+  int chapter = 2;
+  double progress = 0.1;
+  bool completed = false;
+
+  print(course);
+  print(chapter);
+  print(progress);
+  print(completed);
+
+  final String appName = 'My Flutter App';
+
+  final timeNow = DateTime.now();
+  print(timeNow);
+  const double pi = 3.14159; 
+  
+  const minutesInHour = 60;
+  print(minutesInHour);
 }
